74602fa4a4b69a6405c9799707572169959052e9
Carlos <caamaurijes@gmail.com>
2025-10-21 19:55:58 +0000

    testando

--- Iniciante.c
+++ Iniciante.c
@@ -17,17 +17,30 @@
       //criar os 2 navios
      int navio1[3],navio2[3] = {3, 3, 3};  // horizontal vertical
 
-   jogo[9][3] = navio1[0]; //9D  
+   jogo[9][3] = navio1[0]; //9D  // horizontal
    jogo[9][4] = navio1[1]; //9E
    jogo[9][5] = navio1[2]; //9F  
  
-   jogo[5][3] = navio1[0]; //5D  
-   jogo[6][3] = navio1[1]; //6E
-   jogo[7][3] = navio1[2]; //7F 
+   jogo[5][3] = navio2[0]; //5D  // vertical
+   jogo[6][3] = navio2[1]; //6E
+   jogo[7][3] = navio2[2]; //7F 
  
    //exibir tabuleiro e navios
 printf("**Batalha Naval:**\n");
 printf("\n");
+
+printf("A B C D E F G H I J\n"); 
+
+    int linha,coluna;
+
+        for (linha = 0; linha < 10; linha++){
+printf("%d", linha);
+        for (coluna = 0; coluna < 10; coluna++){
+printf("%d", jogo[linha],[coluna]);
+        }
+        
+}
+
 
 
 
